c45a350205f623538bc151f3e5de1998fed3551a
Guwy <onatkutorhan2004@gmail.com>
2023-01-03 15:13:23 +0200

    Initial commit

---
+++ src/main/java/net/guwy/stickyfoundations/StickyFoundations.java
@@ -0,0 +1,55 @@
+package net.guwy.stickyfoundations;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+public class StickyFoundations {
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "sticky_foundations";
+
+    public StickyFoundations() {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        //ModItems.register(eventBus);
+        //ModBlocks.register(eventBus);
+        //ModConfiguredFeatures.register(eventBus);
+        //ModPlacedFeatures.register(eventBus);
+
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
+        eventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){
+        event.enqueueWork(() -> {
+        });
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
