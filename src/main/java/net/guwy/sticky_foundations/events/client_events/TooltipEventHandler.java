da1d86373a8da5a7b5820076dfb5f4075a83e5c6
Guwy <onatkutorhan2004@gmail.com>
2024-01-01 16:43:38 +0200

    + Actually fixed visor gunk tooltip bug with other dynamic injection tooltips

--- src/main/java/net/guwy/sticky_foundations/events/client_events/TooltipEventHandler.java
+++ src/main/java/net/guwy/sticky_foundations/events/client_events/TooltipEventHandler.java
@@ -2,8 +2,16 @@ package net.guwy.sticky_foundations.events.client_events;
 
 import net.guwy.sticky_foundations.index.SFTags;
 import net.guwy.sticky_foundations.mechanics.visor.VisorGunk;
+import net.guwy.sticky_foundations.utils.ItemTagUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TooltipEventHandler {
     public static void init(ItemTooltipEvent event){
