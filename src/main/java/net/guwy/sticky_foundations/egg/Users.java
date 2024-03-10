b20d898b4013009921f15f91a28c3970cab3ff22
Guwy <onatkutorhan2004@gmail.com>
2024-03-10 15:00:04 +0200

    + Backup uuid's for Users.java

--- src/main/java/net/guwy/sticky_foundations/egg/Users.java
+++ src/main/java/net/guwy/sticky_foundations/egg/Users.java
@@ -6,21 +6,27 @@ import net.minecraft.world.entity.player.Player;
 import java.util.UUID;
 
 public enum Users {
-    REDSTONE_STICK("7b11db5a-24b4-4f33-9202-01d0bd9c8d26");
+    REDSTONE_STICK("7b11db5a-24b4-4f33-9202-01d0bd9c8d26", "77f2d061-c9f1-3980-a689-6d047aee96c2");
 
 
     final UUID uuid;
+    final UUID backupUuid;
     final boolean dev;  // temporarily make true for testing
     Users(String uuid){
-        this(uuid, false);
+        this(uuid, null, false);
     }
-    Users(String uuid, boolean dev){
+    Users(String uuid, String backupUuid){
+        this(uuid, backupUuid, false);
+    }
+    Users(String uuid, String backupUuid, boolean dev){
         this.uuid = UUID.fromString(uuid);
+        this.backupUuid = UUID.fromString(backupUuid);;
         this.dev = dev;
     }
 
     public static boolean checkUUID(Player player, Users toCompare) {
         return player.getUUID().equals(toCompare.uuid)
+                || player.getUUID().equals(toCompare.backupUuid)
                 || toCompare.dev;
     }
 }
