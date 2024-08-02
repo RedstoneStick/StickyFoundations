c4618731d066474262700fe2dd5fbf6277eb89e3
Guwy <onatkutorhan2004@gmail.com>
2024-08-02 17:59:40 +0300

    + NeoForge Start

--- src/main/java/net/guwy/sticky_foundations/events/client_events/OnKeyRegisterHandler.java
+++
@@ -1,9 +0,0 @@
-package net.guwy.sticky_foundations.events.client_events;
-
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-
-public class OnKeyRegisterHandler {
-    public static void init(RegisterKeyMappingsEvent event){
-        //event.register(SFKeyBindings.CRAWL_KEY);
-    }
-}
